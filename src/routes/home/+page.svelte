289f0d2ebc9c1869b302527003c2a61ea8742256
aritra2021 <95604384+aritra2021@users.noreply.github.com>
2023-04-28 17:07:59 +0530

    Final touch

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -5,12 +5,14 @@
 </script>
 
 <div bind:clientWidth={width} class="flex {width < 800 ? 'flex-col-reverse -mt-36' : 'flex-row -my-24'} h-full">
-    <div class="flex flex-col items-start justify-center {width < 800 ? 'h-fit' : 'h-full'} w-full py-10 px-4">        
-        <div class="mb-2">Hi, I'm Aritra Mondal.</div>
-        <div class="flex flex-col sm:text-7xl text-5xl text-gray-800 font-extrabold space-y-4">
-            <span class="magical-underline w-fit">Developer. </span>
-            <span class="magical-underline w-fit">Designer. </span>
-            <span class="magical-underline w-fit">Gamer. </span>
+    <div class="flex flex-col items-center justify-center {width < 800 ? 'h-fit' : 'h-full'} w-full">        
+        <div class="py-10 px-4">
+            <div class="mb-2">Hi, I'm Aritra Mondal.</div>
+            <div class="flex flex-col sm:text-7xl text-5xl text-gray-800 font-extrabold space-y-4">
+                <span class="magical-underline w-fit">Developer. </span>
+                <span class="magical-underline w-fit">Designer. </span>
+                <span class="magical-underline w-fit">Gamer. </span>
+            </div>
         </div>
     </div>
     <div class="flex items-center justify-center {width < 800 ? 'h-fit' : 'h-full'} w-full p-4">
