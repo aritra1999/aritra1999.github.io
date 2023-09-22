9722c6821f78a2cce5032019f31e5c3f2102e80e
Aritra Mondal <aritra041999.dev@gmail.com>
2023-09-22 08:50:20 +0200

    Added server data loading for pages

--- src/routes/experience/+page.svelte
+++ src/routes/experience/+page.svelte
@@ -1,19 +1,18 @@
-<script>
-    import AboutCard from "$lib/components/card/AboutCard.svelte";
-    import items from "$lib/data/about.json";
+<script lang="ts">
+  import AboutCard from "$lib/components/card/AboutCard.svelte";
 
-    const experiences = items.experience;
-
+  export let data; 
 </script>
+
 <div class="px-3 lg:px-6 py-10">
-    <div class="my-4">
-        <span class="magical-underline text-3xl font-semibold mb-4">
-            Experience
-        </span>
-        <div class="columns-lg gap-4">
-            {#each experiences as experience}
-                <AboutCard  item={experience}/>
-            {/each}
-        </div>
+  <div class="my-4">
+    <span class="magical-underline text-3xl font-semibold mb-4">
+      Experience
+    </span>
+    <div class="columns-lg gap-4">
+      {#each data.experiences as experience}
+        <AboutCard item={experience} />
+      {/each}
     </div>
+  </div>
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,17 +1,16 @@
 <script lang="ts">
-    import projects from "$lib/data/projects.json";
-    import PLCard from "$lib/components/card/PLCard.svelte";
+  import PLCard from "$lib/components/card/PLCard.svelte";
+
+  export let data;
 </script>
 
 <div class="px-3 lg:px-6 py-10">
-    <h2 class="magical-underline text-3xl font-semibold mb-8 w-fit">
-        Projects
-    </h2>
-    <div class="gap-4 columns-sm">
-        {#each projects as project}
-            <div class="inline-block w-full">
-                <PLCard item={project} />
-            </div>
-        {/each}
-    </div>
+  <h2 class="magical-underline text-3xl font-semibold mb-8 w-fit">Projects</h2>
+  <div class="gap-4 columns-sm">
+    {#each data.projects as project}
+      <div class="inline-block w-full">
+        <PLCard item={project} />
+      </div>
+    {/each}
+  </div>
 </div>
