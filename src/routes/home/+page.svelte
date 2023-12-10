b2acfa791b21982a8ffcebb5c767c26e0be30a40
Aritra Mondal <aritra041999.dev@gmail.com>
2023-12-10 10:32:45 +0100

    Replace static gif with random gif

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,3 +1,7 @@
+<script lang="ts">
+  export let data;
+</script>
+
 <div class="flex xl:flex-row flex-col-reverse h-[calc(100vh-350px)] -mt-6">
   <div class="flex items-center justify-center sm:h-full h-1/2 lg:w-1/2 w-full">
     <div class="px-10">
@@ -14,9 +18,15 @@
   <div
     class="xl:w-1/2 sm:p-0 sm:mt-auto sm:mb-auto mt-10 xl:px-16 md:px-10 px-6 w-full"
   >
-    <img
+    <!-- <img
       src="https://cdn.dribbble.com/users/464600/screenshots/4907524/glasses.gif"
       class="w-full sm:w-2/3 md:w-2/3 lg:w-full mx-auto rounded-t-[50px] rounded-bl-[50px]"
+      alt="me"
+    /> -->
+
+    <img
+      class="rounded-t-[30px] rounded-bl-[30px] mx-auto max-h-[300px]"
+      src={data.gif}
       alt="me"
     />
   </div>
