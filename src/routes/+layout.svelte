37a9ae90e7e28d63741fd559eefb9b38c8275aa7
Aritra <aritra.mondal@leanix.net>
2023-12-22 18:57:08 +0100

    Redesign layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 
   import EmailList from "$lib/components/email/emailList/emailList.svelte";
   import Navbar from "$lib/components/navbar/navbarContainer.svelte";
-  import Topbar from "$lib/components/topbar/topbar.svelte";
   import EmailBody from "$lib/components/email/emailBody/emailBody.svelte";
   import ContactForm from "$lib/components/contact/ContactForm.svelte";
   import { metaStore } from "$lib/store/metaStore";
@@ -19,9 +18,7 @@
 
   $: reactiveClasses = {
     emailListW:
-      $metaStore.screen === "desktop"
-        ? "border-r border-slate-200 w-[40rem]"
-        : "w-full",
+      $metaStore.screen === "desktop" ? "border-r w-[40rem]" : "w-full",
     outerContainerPl:
       $metaStore.showSidebar && $metaStore.screen === "desktop"
         ? "pl-72"
@@ -32,7 +29,7 @@
     outerContainer:
       "fixed w-full pr-2 pb-3 ease-in-out transition-all duration-300 h-[calc(100vh-5rem)]",
     innerContainer:
-      "flex sm:h-[calc(100vh-10rem)] h-[calc(100vh-6rem)] overflow-y-auto overflow-x-hidden w-full border-slate-200",
+      "flex sm:h-full h-[calc(100vh-6rem)] overflow-y-auto overflow-x-hidden w-full border-slate-200 rounded-lg",
   };
 </script>
 
@@ -40,10 +37,9 @@
 <div>
   <Navbar />
   <div class={cn(classes.outerContainer, reactiveClasses.outerContainerPl)}>
-    <div class="bg-background rounded-xl h-full">
-      <Topbar />
+    <div class="bg-background rounded-xl h-full border">
       <div class={cn(classes.innerContainer)}>
-        <div class={cn(reactiveClasses.emailListW, "overflow-y-auto")}>
+        <div class={cn(reactiveClasses.emailListW, "overflow-y-auto p-1")}>
           <EmailList />
         </div>
         <EmailBody>
