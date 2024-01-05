b88655a5fda535fdb5a6505eff1be284fdeebe1d
Aritra <aritra041999.dev@gmail.com>
2024-01-05 18:43:10 +0100

    npm run format with new prettier config

--- src/lib/components/email/emailList/emailList.svelte
+++ src/lib/components/email/emailList/emailList.svelte
@@ -1,31 +1,25 @@
 <script lang="ts">
-	import { page } from "$app/stores";
-	import items from "$lib/data/emailList.json";
-	import { metaStore } from "$lib/store/metaStore";
-	import { cn } from "$lib/utils";
-	import {
-		FileSpreadsheet,
-		FileTerminal,
-		FileText,
-		Paperclip,
-	} from "lucide-svelte";
+	import { page } from '$app/stores';
+	import items from '$lib/data/emailList.json';
+	import { metaStore } from '$lib/store/metaStore';
+	import { cn } from '$lib/utils';
+	import { FileSpreadsheet, FileTerminal, FileText, Paperclip } from 'lucide-svelte';
 
 	const date = new Date();
-	const today = `${date.getDate()} / ${new Intl.DateTimeFormat("en-US", {
-		month: "short",
+	const today = `${date.getDate()} / ${new Intl.DateTimeFormat('en-US', {
+		month: 'short'
 	}).format(date.getMonth())}`;
-	let emailListW =
-		$metaStore.screen === "desktop" ? "border-r w-[40rem]" : "w-fit";
+	let emailListW = $metaStore.screen === 'desktop' ? 'border-r w-[40rem]' : 'w-fit';
 </script>
 
-<div class={cn(emailListW, "overflow-y-auto bg-background")}>
+<div class={cn(emailListW, 'overflow-y-auto border-r bg-background sm:border-r-0')}>
 	{#each Object.entries(items) as [key, item]}
 		<div
 			class="border-b border-zinc-300 px-5 py-4 text-slate-700 hover:bg-zinc-100
       {$page.url.pathname === `/${key}` ? 'bg-white' : ''}"
 		>
 			<a href={`/${key}`}>
-				<div class="flex justify-between mb-2">
+				<div class="mb-2 flex justify-between">
 					<div class="flex items-center">
 						<svg
 							xmlns="http://www.w3.org/2000/svg"
@@ -36,7 +30,7 @@
 							height="32"
 							preserveAspectRatio="xMidYMid meet"
 							viewBox="0 0 24 24"
-							class="h-5 w-5 mr-2"
+							class="mr-2 h-5 w-5"
 							><path
 								fill="currentColor"
 								d="M6.5 17H15l3.5-5L15 7H6.5l3.5 5l-3.5 5m8.5 2H3l4.5-7L3 5h12c.69 0 1.23.3 1.64.86L21 12l-4.36 6.14c-.41.56-.95.86-1.64.86Z"
@@ -44,24 +38,24 @@
 						>
 						<span>{item.title}</span>
 					</div>
-					<div class="flex items-center space-x-2 text-slate-500 text-xs">
+					<div class="flex items-center space-x-2 text-xs text-slate-500">
 						{#if item.attachments.length}
-							<Paperclip class="w-5 h-5" />
+							<Paperclip class="h-5 w-5" />
 						{/if}
 						<span>{today}</span>
 					</div>
 				</div>
-				<div class="text-slate-400 text-sm">{item.content}</div>
+				<div class="text-sm text-slate-400">{item.content}</div>
 				<div class="flex items-center font-medium">
 					{#each item.attachments as attachment}
 						<div
-							class="border-2 border-slate-200 flex px-4 py-2 items-center justify-center rounded-full mt-2 mr-2 hover:bg-blue-50 text-xs text-slate-500"
+							class="mr-2 mt-2 flex items-center justify-center rounded-full border-2 border-slate-200 px-4 py-2 text-xs text-slate-500 hover:bg-blue-50"
 						>
-							{#if attachment.type === "code"}
+							{#if attachment.type === 'code'}
 								<FileTerminal class="mr-2 h-5 w-5  text-blue-400" />
-							{:else if attachment.type === "pdf"}
+							{:else if attachment.type === 'pdf'}
 								<FileText class="mr-2 h-5 w-5 text-red-400" />
-							{:else if attachment.type === "doc"}
+							{:else if attachment.type === 'doc'}
 								<FileSpreadsheet class="mr-2 h-5 w-5" />
 							{/if}
 							{attachment.name}
